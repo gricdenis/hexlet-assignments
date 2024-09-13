67e4bd593fe432097564608733a69d735777726a
Denis Gritsenko <Denis.Gritsenko@raiffeisen.ru>
2024-09-14 02:47:07 +0300

    first commit

---
+++ java-spring-ru/first-start/settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "SpringBootApplication"
+// spring.jpa.generate-ddl = true
+// spring.jpa.hibernate.ddl-auto
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            url = uri(System.getProperty("hexlet_artifactory_plugin_url"))
+            credentials {
+                username = System.getProperty("hexlet_artifactory_user")
+                password = System.getProperty("hexlet_artifactory_password")
+            }
+        }
+    }
+}
